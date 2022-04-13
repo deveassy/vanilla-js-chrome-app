b2826502f9e02b2321d2280a05bfe8bb70527c7c
deveassy <xjfmwld@gmail.com>
2022-04-13 22:55:15 +0900

    추가로 submit 한 경우엔 저장된 localStorage 값에 추가하기

--- js/todos.js
+++ js/todos.js
@@ -2,10 +2,12 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
-const todoArray = [];
+const TODO_KEY = "todos";
+
+let todoArray = [];
 
 function saveTodos() {
-  localStorage.setItem("todos", JSON.stringify(todoArray)); // localStorage에 저장된 값을 불러와서 setting하기 위해 배열모양의 string으로 저장하기 위한 방법
+  localStorage.setItem(TODO_KEY, JSON.stringify(todoArray)); // localStorage에 저장된 값을 불러와서 setting하기 위해 배열모양의 string으로 저장하기 위한 방법
 }
 
 function deleteTodo(event) {
@@ -37,3 +39,11 @@ function handleTodoForm(event) {
 }
 
 todoForm.addEventListener("submit", handleTodoForm);
+
+const savedTodos = localStorage.getItem(TODO_KEY);
+
+if (savedTodos) {
+  const parseTodos = JSON.parse(savedTodos);
+  todoArray = parseTodos; // localStorage에 값이 있을 경우에는 최초 빈 배열인 todoArray에 push를 해서 초기화를 시키지 않도록 parseTodos를 대입시켜줌
+  parseTodos.forEach((item) => paintTodo(item));
+}
